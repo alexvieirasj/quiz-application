c966e4694ee52472e2dba59faa8691cd59ea351e
alexvieirasj <alexvieiracb20@gmail.com>
2021-07-10 03:04:17 -0300

    Ajuste commit

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const form = document.querySelector('.quiz-form')
+/*
+Este exercício será um pouquinho diferente dos anteriores.
 
-const correctAnswers = ['B', 'B', 'B', 'B']
+Seu desafio é desenvolver uma versão do quiz que:
+
+- Aborda um tema diferente (não pode ser de filmes);
+- Tem um tema de cores diferente do que foi apresentado na aula;
+- Exibe na tela a pontuação que o usuário fez. Não há certo ou errado, apenas faça. Essa exibição de pontos é uma das implementações que faremos na próxima aula =D
+
+Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, tente fazer esse exercício sem rever partes da aula.
+
+É importante que a sua versão do quiz seja feita apenas com o conteúdo que vimos até aqui.
+
+Depois de fazer o que foi pedido acima, crie um repositório no GitHub para a sua aplicação e abra uma issue no repositório do curso com:
+
+- O link da sua versão do quiz;
+- Quais foram as suas maiores dúvidas ou dificuldades durante a execução desse exercício;
+- Quais foram as suas menores dificuldades durante a execução desse exercício.
+
+Link do repositório do curso: https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo/issues
+
+Ps: se você não conseguiu fazer tudo o que foi pedido acima, abra a issue mesmo assim =)
+*/
+
+const form = document.querySelector('.quiz-form')
+const divs = document.querySelectorAll('.divs-perguntas')
+
+const correctAnswers = ['A', 'B', 'A', 'B']
 
 form.addEventListener('submit', event => {
     event.preventDefault()
 
     let score = 0
+
     const usersAnswers = [
         form.inputQuestion1.value,
         form.inputQuestion2.value,
@@ -14,12 +40,12 @@ form.addEventListener('submit', event => {
     ]
 
     usersAnswers.forEach((usersAnswer, index) => {
-        
+       
         if(usersAnswer === correctAnswers[index]){
             score += 25
-        }            
+        }  
     })
 
-    console.log(score)
-    
+    alert('Voce acertou ' + score + '% do Quiz')
 })
+
